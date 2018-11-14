a560f1e037b0b1f0ed89921f96f01b8c6f43c19f
SBF <s.b.f@skynet.be>
2018-11-14 20:29:17 +0100

    Fixed #261: Unknown Inverter Type STP4.0-3AV-40

--- SBFspot/version.h
+++ SBFspot/version.h
@@ -1,6 +1,7 @@
 #define VERSION "3.5.0"
 
 // Fixed #230: Unknown Inverter Type SBS3.7-10
+// Fixed #261: Unknown Inverter Type STP4.0-3AV-40
 
 // SBFspotUploadDaemon v2.0.0 - Redesigned for systemd (Thanks to Jonathan de Boyne Pollard)
 // Fixed #215: Start SBFspotUpload.service failed because the control process exited with error code
