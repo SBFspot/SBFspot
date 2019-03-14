09c9bd82bf735987904600bc5c0b5728659a67e9
SBF <s.b.f@skynet.be>
2019-03-14 22:28:49 +0100

    Update version.h

--- SBFspot/version.h
+++ SBFspot/version.h
@@ -4,7 +4,7 @@
 // Fixed #261: Unknown Inverter Type STP4.0-3AV-40
 // Fixed #269: SMA STP6.0-3AV-40 no readout with SBFspot?
 // Fixed #279: Unknown inverter type SMA STP6.0-3AV-40
-// Fixed #181: SBFspotUploadDaemon not uploading
+// Fixed #281: SBFspotUploadDaemon not uploading
 
 /***
 *
