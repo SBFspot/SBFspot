eeb0b031f58b82ea1be280ac9d21d4c5c57622ad
SBF <s.b.f@skynet.be>
2019-06-11 22:43:01 +0200

    Fixed #297 Updated Taglists with new inverters SUNNY BOY 3.0 / 3.6 / 4.0 / 5.0 / 6.0 (SB3.0-1AV-41 / SB3.6-1AV-41 / SB4.0-1AV-41 / SB5.0-1AV-41 / SB6.0-1AV-41)

--- SBFspot/version.h
+++ SBFspot/version.h
@@ -1,11 +1,15 @@
-#define VERSION "3.5.1"
+#define VERSION "3.5.2"
 
+// V3.5.1
 // Fixed #230: Unknown Inverter Type SBS3.7-10
 // Fixed #261: Unknown Inverter Type STP4.0-3AV-40
 // Fixed #269: SMA STP6.0-3AV-40 no readout with SBFspot?
 // Fixed #279: Unknown inverter type SMA STP6.0-3AV-40
 // Fixed #281: SBFspotUploadDaemon not uploading
 // Fixed #287: Unknown Inverter Type: STP5.0-3AV-40
+
+// V3.5.2
+// Fixed #297: 0x000024BA and Inverter Type=SB3.6-1AV-41
 
 /***
 *
