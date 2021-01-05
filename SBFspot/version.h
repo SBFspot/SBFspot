beef66a0bcf98c71afb5a4fd91b40505fb394740
SBF <s.b.f@skynet.be>
2021-01-05 22:43:50 +0100

    Fix #409: Invalid timezone specified (updated date_time_zonespec.csv)

--- SBFspot/version.h
+++ SBFspot/version.h
@@ -3,6 +3,7 @@
 // V3.7.1
 // Fix makefile - make should copy *ALL* .sql files
 // Fix undefined reference to 'boost::system::system_category()' introduced in #394
+// Fix #409: Invalid timezone specified (updated date_time_zonespec.csv)
 
 /***
 *
