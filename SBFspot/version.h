467d8063b4d029fb95c23401f02a437053039a01
SBF <s.b.f@skynet.be>
2021-01-05 23:26:32 +0100

    Fix #403: issue compiling with nosql (undefined reference)

--- SBFspot/version.h
+++ SBFspot/version.h
@@ -3,6 +3,7 @@
 // V3.7.1
 // Fix makefile - make should copy *ALL* .sql files
 // Fix undefined reference to 'boost::system::system_category()' introduced in #394
+// ==> #403: issue compiling with nosql (undefined reference)
 // Fix #409: Invalid timezone specified (updated date_time_zonespec.csv)
 // Fix #412: skipping unresponsive inverter
 // Fix #417: MQTT publishing issue
