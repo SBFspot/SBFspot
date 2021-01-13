4939240522ff8996e535906e5093665ebe528dc3
SBF <s.b.f@skynet.be>
2021-01-13 21:56:12 +0100

    Update version.h

--- SBFspot/version.h
+++ SBFspot/version.h
@@ -2,8 +2,9 @@
 
 /*
 V3.8.0
-	Fix memory leaks
+	Fix memory leaks (see #318)
 	Fix #7 MariaDB10 with Port 3307
+	Fix SQL daydata export (for PVoutput) : Include zero record, just after production stopped
 */
 
 /***
