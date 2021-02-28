931e0ce3f0f2a38c17a2894c9696096d8aed845d
SBF <s.b.f@skynet.be>
2021-02-28 20:34:34 +0100

    Fix #290 Etoday and temperature are shown as ZERO from STP6.0 inverter

--- SBFspot/version.h
+++ SBFspot/version.h
@@ -7,6 +7,7 @@ V3.8.0
 	Fix SQL daydata export (for PVoutput) : Include zero record, just after production stopped
 	Fix #395 Mix of ISO-8859-1 and UTF-8, CRLF and LF line endings
 	Merged #425 Move Inverter related stuff to dedicated class
+	Fix #290 Etoday and temperature are shown as ZERO from STP6.0 inverter
 */
 
 /***
