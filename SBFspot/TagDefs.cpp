9ef1a2a6b5874459187b40aa117732e3c4adc278
SBF <s.b.f@skynet.be>
2021-12-09 22:20:30 +0100

    Cosmetic

--- SBFspot/TagDefs.cpp
+++ SBFspot/TagDefs.cpp
@@ -1,34 +1,34 @@
 /************************************************************************************************
-	SBFspot - Yet another tool to read power production of SMA solar inverters
-	(c)2012-2018, SBF
+    SBFspot - Yet another tool to read power production of SMA solar inverters
+    (c)2012-2021, SBF
 
-	Latest version found at https://github.com/SBFspot/SBFspot
+    Latest version found at https://github.com/SBFspot/SBFspot
 
-	License: Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
-	http://creativecommons.org/licenses/by-nc-sa/3.0/
+    License: Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
+    http://creativecommons.org/licenses/by-nc-sa/3.0/
 
-	You are free:
-		to Share - to copy, distribute and transmit the work
-		to Remix - to adapt the work
-	Under the following conditions:
-	Attribution:
-		You must attribute the work in the manner specified by the author or licensor
-		(but not in any way that suggests that they endorse you or your use of the work).
-	Noncommercial:
-		You may not use this work for commercial purposes.
-	Share Alike:
-		If you alter, transform, or build upon this work, you may distribute the resulting work
-		only under the same or similar license to this one.
+    You are free:
+        to Share - to copy, distribute and transmit the work
+        to Remix - to adapt the work
+    Under the following conditions:
+    Attribution:
+        You must attribute the work in the manner specified by the author or licensor
+        (but not in any way that suggests that they endorse you or your use of the work).
+    Noncommercial:
+        You may not use this work for commercial purposes.
+    Share Alike:
+        If you alter, transform, or build upon this work, you may distribute the resulting work
+        only under the same or similar license to this one.
 
 DISCLAIMER:
-	A user of SBFspot software acknowledges that he or she is receiving this
-	software on an "as is" basis and the user is not relying on the accuracy
-	or functionality of the software for any purpose. The user further
-	acknowledges that any use of this software will be at his own risk
-	and the copyright owner accepts no responsibility whatsoever arising from
-	the use or application of the software.
+    A user of SBFspot software acknowledges that he or she is receiving this
+    software on an "as is" basis and the user is not relying on the accuracy
+    or functionality of the software for any purpose. The user further
+    acknowledges that any use of this software will be at his own risk
+    and the copyright owner accepts no responsibility whatsoever arising from
+    the use or application of the software.
 
-	SMA is a registered trademark of SMA Solar Technology AG
+    SMA is a registered trademark of SMA Solar Technology AG
 
 ************************************************************************************************/
 
@@ -47,136 +47,136 @@ using namespace boost::algorithm;
 
 int TagDefs::readall(std::string path, std::string locale)
 {
-	to_upper(locale); //fix case sensitivity issues on linux systems
+    to_upper(locale); //fix case sensitivity issues on linux systems
 
-	//Build fullpath to taglist<locale>.txt
-	//Default to EN-US if localized file not found
-	std::string fn_taglist = path + "TagList" + locale + ".txt";
+    // Build fullpath to taglist<locale>.txt
+    // Default to EN-US if localized file not found
+    std::string fn_taglist = path + "TagList" + locale + ".txt";
 
-	std::ifstream fs;
-	fs.open(fn_taglist.c_str(), std::ifstream::in);
-	if (!fs.is_open())
+    std::ifstream fs;
+    fs.open(fn_taglist.c_str(), std::ifstream::in);
+    if (!fs.is_open())
     {
-		print_error("Could not open file " + fn_taglist);
-		if (stricmp(locale.c_str(), "EN-US") != 0)
-		{
-			if (isverbose(0)) std::cout << "Using default locale en-US\n";
-			fn_taglist = path + "TagListEN-US.txt";
+        print_error("Could not open file " + fn_taglist);
+        if (stricmp(locale.c_str(), "EN-US") != 0)
+        {
+            if (isverbose(0)) std::cout << "Using default locale en-US\n";
+            fn_taglist = path + "TagListEN-US.txt";
 
-			fs.open(fn_taglist.c_str(), std::ifstream::in);
-			if (!fs.is_open())
-			{
-				print_error("Could not open file " + fn_taglist);
-				return READ_ERROR;
-			}
-		}
-		else return READ_ERROR;
+            fs.open(fn_taglist.c_str(), std::ifstream::in);
+            if (!fs.is_open())
+            {
+                print_error("Could not open file " + fn_taglist);
+                return READ_ERROR;
+            }
+        }
+        else return READ_ERROR;
     }
 
-	std::string line;
-	unsigned int lineCnt = 0;
+    std::string line;
+    unsigned int lineCnt = 0;
 
-	while(getline(fs, line))
-	{
-		lineCnt++;
+    while (getline(fs, line))
+    {
+        lineCnt++;
 
-		//Get rid of comments and empty lines
-		size_t hashpos = line.find_first_of("#\r");
-		if (hashpos != std::string::npos) line.erase(hashpos);
+        //Get rid of comments and empty lines
+        size_t hashpos = line.find_first_of("#\r");
+        if (hashpos != std::string::npos) line.erase(hashpos);
 
-		if (line.size() > 0)
-		{
-			std::string tag_id;
-			std::vector<std::string> tag_info;
-			
-			// Split line at first '='
-			std::string::size_type pos = line.find('=');
-			if (pos != std::string::npos)
-			{
-				tag_id = line.substr(0, pos);
-				std::string tag = line.substr(pos + 1);
-				boost::split(tag_info, tag, [](char c) { return c == '\\'; }); // boost::is_any_of("=") ==> [](char c) { return c == '='; }
-			}
+        if (line.size() > 0)
+        {
+            std::string tag_id;
+            std::vector<std::string> tag_info;
 
-			// Split line Tag\Lri\Descr
-			if (tag_info.size() != 3)
-				print_error("Wrong number of items", lineCnt, fn_taglist);
-			else
-			{
-				bool entryOK = true;
-				unsigned int tagID = 0;
-				try
-				{
-					tagID = boost::lexical_cast<unsigned int>(tag_id);
-				}
-				catch(...)
-				{
-					print_error("Invalid tagID", lineCnt, fn_taglist);
-					entryOK = false;
-				}
+            // Split line at first '='
+            std::string::size_type pos = line.find('=');
+            if (pos != std::string::npos)
+            {
+                tag_id = line.substr(0, pos);
+                std::string tag = line.substr(pos + 1);
+                boost::split(tag_info, tag, [](char c) { return c == '\\'; }); // boost::is_any_of("=") ==> [](char c) { return c == '='; }
+            }
 
-				unsigned int lri = 0;
-				try
-				{
-					lri = boost::lexical_cast<unsigned int>(tag_info[1]);
-				}
-				catch(...)
-				{
-					print_error("Invalid LRI", lineCnt, fn_taglist);
-					entryOK = false;
-				}
+            // Split line Tag\Lri\Descr
+            if (tag_info.size() != 3)
+                print_error("Wrong number of items", lineCnt, fn_taglist);
+            else
+            {
+                bool entryOK = true;
+                unsigned int tagID = 0;
+                try
+                {
+                    tagID = boost::lexical_cast<unsigned int>(tag_id);
+                }
+                catch (...)
+                {
+                    print_error("Invalid tagID", lineCnt, fn_taglist);
+                    entryOK = false;
+                }
 
-				if (entryOK)
-				{
-					std::string tag = tag_info[0];
-					trim(tag);
+                unsigned int lri = 0;
+                try
+                {
+                    lri = boost::lexical_cast<unsigned int>(tag_info[1]);
+                }
+                catch (...)
+                {
+                    print_error("Invalid LRI", lineCnt, fn_taglist);
+                    entryOK = false;
+                }
 
-					std::string descr = tag_info[2];
-					trim(descr);
+                if (entryOK)
+                {
+                    std::string tag = tag_info[0];
+                    trim(tag);
 
-					addTag(tagID, tag, lri, descr);
-				}
-			}
-		}
-	}
+                    std::string descr = tag_info[2];
+                    trim(descr);
 
-	fs.close();
+                    addTag(tagID, tag, lri, descr);
+                }
+            }
+        }
+    }
 
-	return READ_OK;
+    fs.close();
+
+    return READ_OK;
 }
 
 unsigned int TagDefs::getTagIDForLRI(unsigned int LRI)
 {
-	LRI &= 0x00FFFF00;
-	for (std::map<unsigned long, TD>::iterator it=m_tagdefmap.begin(); it!=m_tagdefmap.end(); ++it)
-	{
-		if (LRI == it->second.getLRI())
-			return it->first;
-	}
+    LRI &= 0x00FFFF00;
+    for (std::map<unsigned long, TD>::iterator it = m_tagdefmap.begin(); it != m_tagdefmap.end(); ++it)
+    {
+        if (LRI == it->second.getLRI())
+            return it->first;
+    }
 
-	return 0;
+    return 0;
 }
 
 std::string TagDefs::getTagForLRI(unsigned int LRI)
 {
-	LRI &= 0x00FFFF00;
-	for (std::map<unsigned long, TD>::iterator it=m_tagdefmap.begin(); it!=m_tagdefmap.end(); ++it)
-	{
-		if (LRI == it->second.getLRI())
-			return it->second.getTag();
-	}
+    LRI &= 0x00FFFF00;
+    for (std::map<unsigned long, TD>::iterator it = m_tagdefmap.begin(); it != m_tagdefmap.end(); ++it)
+    {
+        if (LRI == it->second.getLRI())
+            return it->second.getTag();
+    }
 
-	return "";
+    return "";
 }
 
 std::string TagDefs::getDescForLRI(unsigned int LRI)
 {
-	LRI &= 0x00FFFF00;
-	for (std::map<unsigned long, TD>::iterator it=m_tagdefmap.begin(); it!=m_tagdefmap.end(); ++it)
-	{
-		if (LRI == it->second.getLRI())
-			return it->second.getDesc();
-	}
+    LRI &= 0x00FFFF00;
+    for (std::map<unsigned long, TD>::iterator it = m_tagdefmap.begin(); it != m_tagdefmap.end(); ++it)
+    {
+        if (LRI == it->second.getLRI())
+            return it->second.getDesc();
+    }
 
-	return "";
+    return "";
 }
--- SBFspot/TagDefs.h
+++ SBFspot/TagDefs.h
@@ -1,34 +1,34 @@
 /************************************************************************************************
-	SBFspot - Yet another tool to read power production of SMA solar inverters
-	(c)2012-2021, SBF
+    SBFspot - Yet another tool to read power production of SMA solar inverters
+    (c)2012-2021, SBF
 
-	Latest version found at https://github.com/SBFspot/SBFspot
+    Latest version found at https://github.com/SBFspot/SBFspot
 
-	License: Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
-	http://creativecommons.org/licenses/by-nc-sa/3.0/
+    License: Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
+    http://creativecommons.org/licenses/by-nc-sa/3.0/
 
-	You are free:
-		to Share - to copy, distribute and transmit the work
-		to Remix - to adapt the work
-	Under the following conditions:
-	Attribution:
-		You must attribute the work in the manner specified by the author or licensor
-		(but not in any way that suggests that they endorse you or your use of the work).
-	Noncommercial:
-		You may not use this work for commercial purposes.
-	Share Alike:
-		If you alter, transform, or build upon this work, you may distribute the resulting work
-		only under the same or similar license to this one.
+    You are free:
+        to Share - to copy, distribute and transmit the work
+        to Remix - to adapt the work
+    Under the following conditions:
+    Attribution:
+        You must attribute the work in the manner specified by the author or licensor
+        (but not in any way that suggests that they endorse you or your use of the work).
+    Noncommercial:
+        You may not use this work for commercial purposes.
+    Share Alike:
+        If you alter, transform, or build upon this work, you may distribute the resulting work
+        only under the same or similar license to this one.
 
 DISCLAIMER:
-	A user of SBFspot software acknowledges that he or she is receiving this
-	software on an "as is" basis and the user is not relying on the accuracy
-	or functionality of the software for any purpose. The user further
-	acknowledges that any use of this software will be at his own risk
-	and the copyright owner accepts no responsibility whatsoever arising from
-	the use or application of the software.
+    A user of SBFspot software acknowledges that he or she is receiving this
+    software on an "as is" basis and the user is not relying on the accuracy
+    or functionality of the software for any purpose. The user further
+    acknowledges that any use of this software will be at his own risk
+    and the copyright owner accepts no responsibility whatsoever arising from
+    the use or application of the software.
 
-	SMA is a registered trademark of SMA Solar Technology AG
+    SMA is a registered trademark of SMA Solar Technology AG
 
 ************************************************************************************************/
 
@@ -46,56 +46,56 @@ extern int verbose;
 class TagDefs
 {
 public:
-	enum {READ_OK = 0, READ_ERROR = -1};
+    enum { READ_OK = 0, READ_ERROR = -1 };
 
-	// Tag ID's
-	enum {DEG_C = 2, DEG_F = 32};
-
-private:
-	class TD
-	{
-	private:
-		std::string m_tag;		// Label
-		unsigned int m_lri;	// Logical Record Index
-		std::string m_desc;		// Description
-
-	public:
-		TD() { }
-		TD(std::string tag, unsigned int lri, std::string desc) : m_tag(tag), m_lri(lri), m_desc(desc) {}
-		std::string getTag() const { return m_tag; }
-		unsigned int getLRI() const { return m_lri; }
-		std::string getDesc() const { return m_desc; }
-	};
+    // Tag ID's
+    enum { DEG_C = 2, DEG_F = 32 };
 
 private:
-	std::map<unsigned long, TD> m_tagdefmap;
+    class TD
+    {
+    private:
+        std::string m_tag;  // Label
+        unsigned int m_lri; // Logical Record Index
+        std::string m_desc; // Description
+
+    public:
+        TD() { }
+        TD(std::string tag, unsigned int lri, std::string desc) : m_tag(tag), m_lri(lri), m_desc(desc) {}
+        std::string getTag() const { return m_tag; }
+        unsigned int getLRI() const { return m_lri; }
+        std::string getDesc() const { return m_desc; }
+    };
 
 private:
-	bool isverbose(int level)
-	{
-		return !quiet && (verbose >= level);
-	}
-	void print_error(std::string msg)
-	{
-		std::cerr << "Error: " << msg << std::endl;
-	}
-	void print_error(std::string msg, unsigned int line, std::string fpath)
-	{
-		std::cerr << "Error: " << msg << " on line " << line << " [" << fpath << "]\n";
-	}
-	void addTag(unsigned int tagID, std::string tag, unsigned int lri, std::string desc)
-	{
-		m_tagdefmap.insert(std::make_pair(tagID, TD(tag, lri, desc)));
-	}
+    std::map<unsigned long, TD> m_tagdefmap;
+
+private:
+    bool isverbose(int level)
+    {
+        return !quiet && (verbose >= level);
+    }
+    void print_error(std::string msg)
+    {
+        std::cerr << "Error: " << msg << std::endl;
+    }
+    void print_error(std::string msg, unsigned int line, std::string fpath)
+    {
+        std::cerr << "Error: " << msg << " on line " << line << " [" << fpath << "]\n";
+    }
+    void addTag(unsigned int tagID, std::string tag, unsigned int lri, std::string desc)
+    {
+        m_tagdefmap.insert(std::make_pair(tagID, TD(tag, lri, desc)));
+    }
 
 public:
-	int readall(std::string path, std::string locale);
-	std::string getTag(unsigned int tagID) { return m_tagdefmap[tagID].getTag(); }
-	unsigned int getTagIDForLRI(unsigned int LRI);
-	std::string getTagForLRI(unsigned int LRI);
-	std::string getDescForLRI(unsigned int LRI);
-	unsigned int getLRI(unsigned int tagID) { return m_tagdefmap[tagID].getLRI(); }
-	std::string getDesc(unsigned int tagID) { return m_tagdefmap[tagID].getDesc(); }
-	std::string getDesc(unsigned int tagID, std::string _default) { return m_tagdefmap[tagID].getDesc().empty() ? _default : m_tagdefmap[tagID].getDesc(); }
-	std::map<unsigned int, TD>::size_type size(void) const { return m_tagdefmap.size(); }
+    int readall(std::string path, std::string locale);
+    std::string getTag(unsigned int tagID) { return m_tagdefmap[tagID].getTag(); }
+    unsigned int getTagIDForLRI(unsigned int LRI);
+    std::string getTagForLRI(unsigned int LRI);
+    std::string getDescForLRI(unsigned int LRI);
+    unsigned int getLRI(unsigned int tagID) { return m_tagdefmap[tagID].getLRI(); }
+    std::string getDesc(unsigned int tagID) { return m_tagdefmap[tagID].getDesc(); }
+    std::string getDesc(unsigned int tagID, std::string _default) { return m_tagdefmap[tagID].getDesc().empty() ? _default : m_tagdefmap[tagID].getDesc(); }
+    std::map<unsigned int, TD>::size_type size(void) const { return m_tagdefmap.size(); }
 };
